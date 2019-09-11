c0c074000c21e6277cacc4e321faee24a482184e
d7p4n4 <bercikezs199999999@gmail.com>
2019-09-11 21:49:30 +0200

    1.20190911.4

--- SecondTry/Program.cs
+++ SecondTry/Program.cs
@@ -7,14 +7,13 @@ namespace SecondTry
 {
     class Program
     {
-        static readonly string textFile = Path.Combine(Environment.CurrentDirectory, "Person.java");
         static void Main(string[] args)
         {
             var map = new Dictionary<string, string>();
             map.Add("name", "String");
             map.Add("guid", "String");
 
-            string[] text = File.ReadAllLines(textFile);
+            string[] text = ReadIn.readIn("Template");
             int x = text.Length;
 
             WriteOut.writeOut(Generate.generateClass(text, "com.d7p4n4", "Person", map, x), "Person3");
---
+++ SecondTry/ReadIn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondTry
+{
+    class ReadIn
+    {
+        public static string[] readIn(string fileName)
+        {
+
+            string textFile = Path.Combine(Environment.CurrentDirectory, fileName + ".java");
+
+            string[] text = File.ReadAllLines(textFile);
+
+            return text;
+        }
+    }
+}
